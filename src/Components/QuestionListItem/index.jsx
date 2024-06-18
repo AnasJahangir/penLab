c10bc46111893a237b76df2691c77ed17167b366
Anas Jahangir <md4909719@gmail.com>
2024-06-18 12:55:56 +0500

    d

--- src/Components/QuestionListItem/index.jsx
+++ src/Components/QuestionListItem/index.jsx
@@ -12,6 +12,7 @@ const QuestionListItem = ({ question }) => {
   };
 
   return (
+
     <div className="relative">
       <li className="flex justify-between items-center mb-4">
         <div className="cursor-pointer" onClick={() => navigate("/questions")}>
